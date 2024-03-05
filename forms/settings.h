b47ad01861b7cff0955070b99453475b94948b8a
andersonsouza <andersom.eugenio@gmail.com>
2024-03-05 20:05:23 -0300

    primeiro commit

---
+++ forms/settings.h
@@ -0,0 +1,35 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QDialog>
+#include <QSettings>
+
+#include "src/modbuscommsettings.h"
+
+namespace Ui {
+    class Settings;
+}
+
+class Settings : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Settings(QWidget *parent = 0 ,ModbusCommSettings * settings = 0);
+    ~Settings();
+    bool modbus_connected;
+
+private:
+    Ui::Settings *ui;
+    ModbusCommSettings *m_settings;
+
+private slots:
+    void changesAccepted();
+
+
+protected:
+    void showEvent(QShowEvent * event);
+
+};
+
+#endif // SETTINGS_H
---
+++ forms/settingsmodbusrtu.h
@@ -0,0 +1,34 @@
+#ifndef SETTINGSMODBUSRTU_H
+#define SETTINGSMODBUSRTU_H
+
+#include <QDialog>
+#include <QSettings>
+
+#include "src/modbuscommsettings.h"
+
+namespace Ui {
+    class SettingsModbusRTU;
+}
+
+class SettingsModbusRTU : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsModbusRTU(QWidget *parent = 0 ,ModbusCommSettings * settings = 0);
+    ~SettingsModbusRTU();
+    bool modbus_connected;
+
+private:
+    Ui::SettingsModbusRTU *ui;
+    ModbusCommSettings * m_settings;
+
+private slots:
+    void changesAccepted();
+
+protected:
+    void showEvent(QShowEvent * event);
+
+};
+
+#endif // SETTINGSMODBUSRTU_H
